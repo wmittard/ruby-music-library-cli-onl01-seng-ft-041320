7a3847db1a833c923e936a41925e181e6bd13859
William Mittard <billymittard@ymail.com>
2020-05-02 15:48:57 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -6,7 +6,7 @@ module Concerns::Findable
     end 
     
     def find_or_create_by_name(name)
-      binding.pry
+      #binding.pry
       self.find_by_name(name) || self.create(name)
     end 
 end 
