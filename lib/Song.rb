7d4f1080ed03622b136e7740ac4cfbb48332fff0
William Mittard <billymittard@ymail.com>
2020-05-02 15:03:47 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -9,7 +9,7 @@ class Song
   @@all = [] 
   
   def initialize(name,artist=nil,genre=nil)
-    @name = name.split(" - ") 
+    @name = name 
     self.artist= artist if artist 
     self.genre= genre  if genre 
     save
