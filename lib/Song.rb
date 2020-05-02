b1b218cea7522f4828b464b3b14a30b328066908
William Mittard <billymittard@ymail.com>
2020-05-02 14:43:28 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -47,11 +47,11 @@ class Song
     genre.songs << self unless genre.songs.include?(self)
   end 
   
-  def self.find_by_name(name)
+  def find_by_name(name)
   @@all.detect {|find| find.name == name}
 end
  
-def self.find_or_create_by_name(name)
+def find_or_create_by_name(name)
    self.find_by_name(name) || self.create(name)
 end
 
