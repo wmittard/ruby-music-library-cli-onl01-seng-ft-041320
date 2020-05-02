62975f4a6da14f16647f2475a8e38e930de82320
William Mittard <billymittard@ymail.com>
2020-05-02 15:28:50 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -52,7 +52,7 @@ class Song
 end
  
 def self.find_or_create_by_name(name)
-    self.find_by_name.uniq(name) || self.create(name)
+    self.find_by_name(name) || self.create(name)
     #
   end 
   
