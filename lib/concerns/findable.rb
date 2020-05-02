73657930da0cee50806e917cda9e93278255276e
William Mittard <billymittard@ymail.com>
2020-05-02 15:52:46 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 module Concerns::Findable
   attr_accessor :name 
   
-   def find_by_name(name)
-   @@all.detect {|find| find.name == name}
+  def find_by_name(name)
+   self.all.detect {|find| find.name == name}
     end 
     
     def find_or_create_by_name(name)
