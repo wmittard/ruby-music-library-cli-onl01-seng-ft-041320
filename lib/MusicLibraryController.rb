99cf58bd974e73583a660381ae06b95bcd90c0c0
William Mittard <billymittard@ymail.com>
2020-05-02 14:24:02 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -44,52 +44,7 @@ class MusicLibraryController
      end
    end
 
-  def list_songs
-        Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
-            puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-        end
-    end
-
-    def list_artists
-     Artist.all.each {|artist| puts artist.name}
-   end
-
-    def list_genres
-     Genre.all.each {|genre| puts genre.name}
-   end
  
-    
-  def list_songs_by_artist
-    puts "Please enter the name of an artist:"
-    input = gets.strip
-
-    if artist = Artist.find_by_name(input)
-      artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
-        puts "#{i+1}. #{s.name} - #{s.genre.name}"
-      end
-    end
-  end
-
-  def list_songs_by_genre
-    puts "Please enter the name of a genre:"
-    input = gets.strip
-
-    if genre = Genre.find_by_name(input)
-      genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
-        puts "#{i+1}. #{s.artist.name} - #{s.name}"
-      end
-    end
-    end
-
-  def play_song
-    puts "Which song number would you like to play?"
-    input = gets.strip.to_i
-    if input > 0 && input <= Song.all.length
-      array = Song.all.sort{|a, b| a.name <=> b.name}
-      song = array[input-1]
-      puts "Playing #{song.name} by #{song.artist.name}"
-    end
-  end
 
     
     
