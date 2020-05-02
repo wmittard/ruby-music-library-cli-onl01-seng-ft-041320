04e45a2ade06bc4211f74fb5aff9fde4bafd5e6f
William Mittard <billymittard@ymail.com>
2020-05-02 15:48:10 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,18 +1,6 @@
 module Concerns::Findable
   attr_accessor :name 
   
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-   
-   
    def find_by_name(name)
    @@all.detect {|find| find.name == name}
     end 
