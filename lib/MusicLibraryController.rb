0b5fecfe6ef6649a666c9dd15ad2a20ae6c4b453
William Mittard <billymittard@ymail.com>
2020-05-02 14:25:55 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -4,8 +4,8 @@ class MusicLibraryController
    attr_reader :path 
   
   def initialize(path ='./db/mp3s')
-     @path = path
-     MusicImporter.new(path).import 
+      @path = path
+     get_importer = MusicImporter.new(path)
    end 
   
      
