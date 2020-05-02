0ce0eb1cf089b3a47766f4d2bacec726463a7a16
William Mittard <billymittard@ymail.com>
2020-05-02 15:15:00 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -45,9 +45,7 @@ class MusicLibraryController
 
     end
 
- 
-
- def list_songs
+  def list_songs
         Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
             puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
         end
